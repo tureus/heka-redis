db709c47a5e580a480307f561947792e6d322016
Xavier Lange <xrlange@gmail.com>
2015-06-15 14:07:33 -0700

    Batch size support

--- list_input.go
+++ list_input.go
@@ -12,15 +12,17 @@ type RedisListInputConfig struct {
 	Address     string `toml:"address"`
 	ListName    string `toml:"listname"`
 	DecoderName string `toml:"decoder"`
+	BatchSize   int    `toml:"batchsize"`
 }
 
 type RedisListInput struct {
 	conf *RedisListInputConfig
 	conn redis.Conn
+	batchlpop *redis.Script
 }
 
 func (rli *RedisListInput) ConfigStruct() interface{} {
-	return &RedisListInputConfig{":6379", "logs", ""}
+	return &RedisListInputConfig{":6379", "logs", "", 1}
 }
 
 func (rli *RedisListInput) Init(config interface{}) error {
@@ -32,6 +34,23 @@ func (rli *RedisListInput) Init(config interface{}) error {
 		return fmt.Errorf("connecting to - %s", err.Error())
 	}
 
+  rli.batchlpop = redis.NewScript(1, `
+	  local i = tonumber(ARGV[1])
+	  local res = {}
+
+	  local length = redis.call('llen',KEYS[1])
+	  if length < i then i = length end
+	  while (i > 0) do
+	    local item = redis.call("lpop", KEYS[1])
+	    if (not item) then
+	      break
+	    end
+	    table.insert(res, item)
+	    i = i-1
+	  end
+	  return res
+	`)
+
 	return nil
 }
 
@@ -39,12 +58,12 @@ func (rli *RedisListInput) Run(ir pipeline.InputRunner, h pipeline.PluginHelper)
 	var (
 		dRunner pipeline.DecoderRunner
 		decoder pipeline.Decoder
-		pack    *pipeline.PipelinePack
-		e       error
 		ok      bool
+		e       error
+		reply   interface{}
+		vals    []string
+		msg     string
 	)
-	// Get the InputRunner's chan to receive empty PipelinePacks
-	packSupply := ir.InChan()
 
 	if rli.conf.DecoderName != "" {
 		if dRunner, ok = h.DecoderRunner(rli.conf.DecoderName, fmt.Sprintf("%s-%s", ir.Name(), rli.conf.DecoderName)); !ok {
@@ -54,46 +73,68 @@ func (rli *RedisListInput) Run(ir pipeline.InputRunner, h pipeline.PluginHelper)
 	}
 
 	for {
-		reply, err := rli.conn.Do("BLPOP", rli.conf.ListName, "0")
+		reply, e = rli.conn.Do("BLPOP", rli.conf.ListName, "0")
+		if e == nil {
+			vals, e = redis.Strings(reply, nil)
+			msg = vals[1]
 
-		if err == nil {
-			vals, extracterr := redis.Strings(reply, nil)
+			if e == nil {
+				rli.InsertMessage(ir, decoder, msg)
+			}
+		}
 
-			if extracterr == nil {
+		reply, e = rli.batchlpop.Do(rli.conn, rli.conf.ListName, rli.conf.BatchSize)
 
-				pack = <-packSupply
-				pack.Message.SetType("redis_list")
-				// pack.Message.SetLogger(n.Channel)
-				pack.Message.SetPayload(vals[1])
-				pack.Message.SetTimestamp(time.Now().UnixNano())
+		if e == nil {
+			vals, e = redis.Strings(reply, nil)
 
-				var packs []*pipeline.PipelinePack
-				if decoder == nil {
-					packs = []*pipeline.PipelinePack{pack}
-				} else {
-					packs, e = decoder.Decode(pack)
-				}
-
-				if packs != nil {
-					for _, p := range packs {
-						ir.Inject(p)
-					}
-				} else {
-					if e != nil {
-						ir.LogError(fmt.Errorf("Couldn't parse Redis message: %s", vals))
-					}
-					pack.Recycle()
+			if e == nil {
+				for _,msg = range vals {
+					rli.InsertMessage(ir, decoder, msg)
 				}
 			} else {
-				fmt.Printf("err: %v\n", extracterr)
+				fmt.Printf("err: %v\n", e)
 			}
 		} else {
-			fmt.Printf("type: %T, error: %v\n", reply, err)
-			return err
+			fmt.Printf("type: %T, error: %v\n", reply, e)
+			return e
 		}
 	}
 
 	return nil
+}
+
+func (rli *RedisListInput) InsertMessage(ir pipeline.InputRunner, decoder pipeline.Decoder, msg string) {
+	var (
+		pack    *pipeline.PipelinePack
+		e error
+	)
+	// Get the InputRunner's chan to receive empty PipelinePacks
+	packSupply := ir.InChan()
+
+	pack = <-packSupply
+	pack.Message.SetType("redis_list")
+	// pack.Message.SetLogger(n.Channel)
+	pack.Message.SetPayload(msg)
+	pack.Message.SetTimestamp(time.Now().UnixNano())
+
+	var packs []*pipeline.PipelinePack
+	if decoder == nil {
+		packs = []*pipeline.PipelinePack{pack}
+	} else {
+		packs, e = decoder.Decode(pack)
+	}
+
+	if packs != nil {
+		for _, p := range packs {
+			ir.Inject(p)
+		}
+	} else {
+		if e != nil {
+			ir.LogError(fmt.Errorf("Couldn't parse %s", msg))
+		}
+		pack.Recycle()
+	}
 }
 
 func (rli *RedisListInput) Stop() {
